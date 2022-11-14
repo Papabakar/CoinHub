28899b3364d120189fa9dc9aa4af6900d19ec4e3
Miles Morales <milesmorales2250@gmail.com>
2022-11-14 18:15:33 +0000

    add marquee

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
+import CoinList from './components/coinlist/coin-list';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <CoinList /> 
     </div>
   );
 }
