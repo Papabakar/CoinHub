90728cc92a78868fe020627e20aadff2fef456b8
Miles <webdevmiles@gmail.com>
2023-01-09 22:12:44 +0000

    push new code

--- src/components/home/home-page.jsx
+++ src/components/home/home-page.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import CoinList from "../Coinlist/coin";
 import Hero from "../../routes/hero/hero";
 import HeroText from "../HeroText/hero-text";
-import FooterMenu from '../footer/footer';
+import FooterMenu from '../Footer/footer';
 import Intro from "../../routes/intro";
 import NewsSlider from "../../routes/news-slider";
 import Main from "../../routes/main";
-import Blog from '../blog/blog';
+import Blog from '../Blog/blog';
 import Newsletter from "../newsletter/newsletter";
 import HotStories from "../hot-stories/hot-stories";
 import BlogNewsletter from '../newsletter/newsletter';
