c87e2959272cb50fe2c2644a5519454a8020491f
Miles <webdevmiles@gmail.com>
2023-01-09 22:07:04 +0000

    adding gsap

--- src/snippets/article-main/article-title.jsx
+++ src/snippets/article-main/article-title.jsx
@@ -1,4 +1,5 @@
 import { format } from "date-fns";
+import {ReactComponent as Calendar} from '../../assets/svgs/calendar.svg'
 
 const ArticleInfo = ({ articleInfoData }) => {
   const { author, postDate } = articleInfoData;
@@ -7,8 +8,8 @@ const ArticleInfo = ({ articleInfoData }) => {
     <div className="flex items-center w-full justify-between space-x-3 text-gray-500 dark:text-gray-400 text-base mb-2 lg:mb-0">
       <span>By {author}</span>
       <span>
-        <span className="font-normal text-gray-500 dark:text-gray-400">
-          {format(new Date(postDate), "yyyy-MM-dd")}
+        <span className="font-normal text-gray-500 dark:text-gray-400 flex items-center justify-center gap-1">
+         <Calendar /> {format(new Date(postDate), "yyyy-MM-dd")}
         </span>
       </span>
     </div>
