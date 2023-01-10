8eec3a4504c7e1539fe5ab71c07d20ae34c52ef3
Ola4606 <olaoluwaadeyemo7@gmail.com>
2023-01-10 20:32:42 +0100

    Nav Links

--- src/components/home/home-page.jsx
+++ src/components/home/home-page.jsx
@@ -1,33 +1,47 @@
-import React from 'react'
+import React from "react";
 import CoinList from "../Coinlist/coin";
 import Hero from "../../routes/hero/hero";
 import HeroText from "../HeroText/hero-text";
-import FooterMenu from '../footer/footer';
+import FooterMenu from "../footer/footer";
 import Intro from "../../routes/intro";
 import NewsSlider from "../../routes/news-slider";
 import Main from "../../routes/main";
-import Blog from '../blog/blog';
+import Blog from "../blog/blog";
 import Newsletter from "../newsletter/newsletter";
 import HotStories from "../hot-stories/hot-stories";
-import BlogNewsletter from '../newsletter/newsletter';
-import NewsMarquee from '../news-marquee/news-marquee';
-import { NEWSSLIDERDATA } from '../../data/document.data';
+import BlogNewsletter from "../newsletter/newsletter";
+import NewsMarquee from "../news-marquee/news-marquee";
+import { NEWSSLIDERDATA } from "../../data/document.data";
+import { useRef } from "react";
 
 function HomePage() {
   return (
     <div className="overflow-x-hidden flex flex-col gap-20">
-
-        <Hero />
-        <HeroText />
-        <Intro />
-        <NewsMarquee data={NEWSSLIDERDATA} sliderLine={"snippetLine"} sliderBox={"snippetBox"} direction={1} speed={0.1} gap={20} />
-        <Main />
-        <NewsMarquee data={NEWSSLIDERDATA} sliderLine={"snippetLine_2"} sliderBox={"snippetBox_2"} direction={-1} speed={0.1} gap={20} />
-        <Blog />
-        <HotStories />
-        <FooterMenu />
+      <Hero />
+      <HeroText />
+      <Intro />
+      <NewsMarquee
+        data={NEWSSLIDERDATA}
+        sliderLine={"snippetLine"}
+        sliderBox={"snippetBox"}
+        direction={1}
+        speed={0.1}
+        gap={20}
+      />
+      <Main />
+      <NewsMarquee
+        data={NEWSSLIDERDATA}
+        sliderLine={"snippetLine_2"}
+        sliderBox={"snippetBox_2"}
+        direction={-1}
+        speed={0.1}
+        gap={20}
+      />
+      <Blog />
+      <HotStories />
+      <FooterMenu />
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
--- src/components/navmenu/sub-components/navlinks/navlinks.jsx
+++ src/components/navmenu/sub-components/navlinks/navlinks.jsx
@@ -1,19 +1,26 @@
+const NavLinks = ({ navLinks }) => {
+  return (
+    <ul className="md:flex hidden gap-3.5">
+      {navLinks.map((item) => {
+        const { name, id, route } = item;
 
-const NavLinks = ({navLinks}) => {
-    return(
-        <ul className="md:flex hidden gap-3.5">
-            {navLinks.map((item) => {
-                const {name, id, route} = item
-                return(
-                    <a className="text-white text-md" key={id} href={route}>
-                        <li>
-                            {name}
-                        </li>
-                    </a>
-                ) 
-            })}
-        </ul>
-    )
-}
+        return (
+          <a
+            className="text-white text-md"
+            key={id}
+            onClick={() => {
+              const element = document.getElementById(route);
+              if (element) {
+                element.scrollIntoView({ behavior: "smooth" });
+              }
+            }}
+          >
+            <li>{name}</li>
+          </a>
+        );
+      })}
+    </ul>
+  );
+};
 
-export default NavLinks
+export default NavLinks;
