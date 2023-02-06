b2f358c49db9b9bcf148c23e771c5955668dd008
daddyloyal <lifeofemmany6@gmail.com>
2023-02-06 23:28:16 +0100

    Made some changes to the market spotter and added a redirect link to the main market spotter site

--- src/components/Main/main-text.jsx
+++ src/components/Main/main-text.jsx
@@ -1,18 +1,26 @@
-
+// import handDown from '../../assets/svgs/hand-down.svg';
 const MainText = () => {
-    return(
-        <div className="flex flex-col gap-5">
-            <div className="">
-                <h2 className="font-bold text-4xl text-white">Market Spotter</h2>
-            </div>
+  return (
+    <div className="flex flex-col gap-5">
+      <div className="">
+        <h2 className="font-bold text-4xl text-white">Market Spotter</h2>
+      </div>
 
-            <p className="text-gray-400 max-w-[60ch]">Market Spotter was created to remove frustration when analyzing markets. We found that too many inexperienced traders where cluttering their charts with countless indicators, resulting in confusion when analyzing price action. Market Spotter provides simple, easy to use indicators with unbiased buy and sell opportunities.</p>
+      <p className="text-gray-400 max-w-[60ch]">
+        Are you tired of constantly monitoring the market for the right time to
+        make a trade? Look no further! 'Market Spotter' offers advanced
+        algorithms and real-time data analysis for informed trades. Say goodbye
+        to chart analysis, hello to profits. Try it today! (Learn More) referral
+        link
+      </p>
 
-        <div className="">
-                <a href="" className="nav-primary text-white">Learn More</a>
-            </div>
-        </div>
-    )
-}
+      <div className="">
+        <a href="https://marketspotter.io?refferal=@papabakar_1392" target="_blank" className="nav-primary text-white">
+          Learn More
+        </a>
+      </div>
+    </div>
+  );
+};
 
-export default MainText
+export default MainText;
