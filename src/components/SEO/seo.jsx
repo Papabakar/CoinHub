8659911da1f7fdc19ceab28fda4e9bd9a1266f8b
puneet kumar <puneet.softweb@gmail.com>
2023-02-18 02:11:03 +0530

    meta

--- src/components/SEO/seo.jsx
+++ src/components/SEO/seo.jsx
@@ -18,23 +18,23 @@ class SEO extends React.Component {
             <meta name="twitter:image" content={this.props.img ? this.props.img : Logohero}></meta> */}
 
 
-            <title>{this.props.title}</title>
-            <meta name="description" content={this.props.desc} />
+            <title>{this.props.title ? this.props.title : 'CoinHub | Fastest real-time crypto news source.'}</title>
+            <meta name="description" content={this.props.desc ? this.props.desc : ''} />
 
             {/* <!-- Google / Search Engine Tags --> */}
-            <meta itemprop="name" content={this.props.title} />
-            <meta itemprop="description" content={this.props.desc} />
+            <meta itemprop="name" content={this.props.title ? this.props.title : 'CoinHub | Fastest real-time crypto news source.'} />
+            <meta itemprop="description" content={this.props.desc ? this.props.desc : 'CoinHub is the fastest real-time crypto news source covering cryptocurrency, Bitcoin, Ethereum, Defi and more.'} />
             <meta itemprop="image" content={this.props.img}/>
 
             {/* <!-- Facebook Meta Tags --> */}
-            <meta property="og:title" content={this.props.title} />
-            <meta property="og:description" content={this.props.desc} />
+            <meta property="og:title" content={this.props.title ? this.props.title : 'CoinHub | Fastest real-time crypto news source.'} />
+            <meta property="og:description" content={this.props.desc ? this.props.desc : 'CoinHub is the fastest real-time crypto news source covering cryptocurrency, Bitcoin, Ethereum, Defi and more.'} />
             <meta property="og:image" content={this.props.img} />
 
             {/* <!-- Twitter Meta Tags --> */}
-            <meta name="twitter:title" content={this.props.title} />
-            <meta name="twitter:description" content={this.props.desc} />
-            <meta name="twitter:image" content={this.props.img} />
+            <meta name="twitter:title" content={this.props.title ? this.props.title : 'CoinHub | Fastest real-time crypto news source.'} />
+            <meta name="twitter:description" content={this.props.desc ? this.props.desc : 'CoinHub is the fastest real-time crypto news source covering cryptocurrency, Bitcoin, Ethereum, Defi and more.'} />
+            <meta name="twitter:image" content={this.props.img ? this.props.img : Logohero} />
         </Helmet>
     );
   }
