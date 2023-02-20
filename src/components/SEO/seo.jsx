4a7c886821d9ab735da4288a1fbdfca92a2fed54
puneet kumar <puneet.softweb@gmail.com>
2023-02-20 18:01:13 +0530

    seo

--- src/components/SEO/seo.jsx
+++ src/components/SEO/seo.jsx
@@ -18,28 +18,29 @@ class SEO extends React.Component {
             <meta name="twitter:image" content={this.props.img}></meta> */}
 
 
-            <title>{this.props.title}</title>
-            <meta name="description" content={this.props.desc} />
 
             {/* <!-- Google / Search Engine Tags --> */}
-            <meta itemprop="name" content={this.props.title} />
+            {/* <meta itemprop="name" content={this.props.title} />
             <meta itemprop="description" content={this.props.desc} />
-            <meta itemprop="image" content={this.props.img}/>
+          <meta itemprop="image" content={this.props.img}/> */}
 
             {/* <!-- Facebook Meta Tags --> */}
-            <meta property="og:title" content={this.props.title} />
+            {/* <meta property="og:title" content={this.props.title} />
             <meta property="og:description" content={this.props.desc} />
-            <meta property="og:image" content={this.props.img} />
+            <meta property="og:image" content={this.props.img} /> */}
 
             {/* <!-- Twitter Meta Tags --> */}
-            <meta name="twitter:title" content={this.props.title} />
-            <meta name="twitter:description" content={this.props.desc} />
-            <meta name="twitter:image" content={this.props.img} />
+            {/* <meta name="twitter:title" content={this.props.title} />
+            <meta name="twitter:description" content={this.props.desc} /> */}
 
 
+            <title>{this.props.title}</title>
+            <meta name="description" content={this.props.desc} />
             <meta property="og:url" content={this.props.url} />
-            <meta property="og:image:width" content="1920" />
-            <meta property="og:image:height" content="1080" />
+            <meta property="og:type" content={this.props.title} />
+            <meta property="og:title" content={this.props.title} />
+            <meta property="og:description" content={this.props.desc} />
+            <meta name="twitter:image" content={this.props.img} />
         </Helmet>
     );
   }
